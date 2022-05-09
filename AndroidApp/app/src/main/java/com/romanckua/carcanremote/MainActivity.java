17ba189c23a410abeddfbf9220d792b7faf98dcf
Roman Pushenko <romanckua@gmail.com>
2022-05-09 20:55:05 +0300

    add Check permissions in app

--- AndroidApp/app/src/main/java/com/romanckua/carcanremote/MainActivity.java
+++ AndroidApp/app/src/main/java/com/romanckua/carcanremote/MainActivity.java
@@ -3,6 +3,8 @@ package com.romanckua.carcanremote;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -19,5 +21,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new BluetoothPermits(this).verificationOfPermits();
+        new GeolocationPermits(this).verificationOfPermits();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 17890) {
+            new BluetoothPermits(this).verificationOfPermits();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 17990) {
+            new GeolocationPermits(this).verificationOfPermits();
+        }
+
     }
 }
