05f9e58558b524c2bfcdf7f6e784d2b2638d25c4
Roman Pushenko <romanckua@gmail.com>
2022-05-09 21:12:20 +0300

    add bluetooth scanner

--- AndroidApp/app/src/main/java/com/romanckua/carcanremote/MainActivity.java
+++ AndroidApp/app/src/main/java/com/romanckua/carcanremote/MainActivity.java
@@ -3,12 +3,17 @@ package com.romanckua.carcanremote;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String BROADCAST_ACTION = "com.romanckua.carcanremote";
+    public static final String PARAM_MSG = "CONTROL";
 
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
@@ -43,4 +48,28 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_car) {
+            // Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+            // intent.putExtra("ScanBluetoothActivity", bluetoothScanMethod);
+            //startActivity(intent);
+            return true;
+        }
+        if (id == R.id.action_scanbt) {
+            // Intent intent = new Intent(MainActivity.this, ScanBluetoothActivity.class);
+            // intent.putExtra("ScanBluetoothActivity", bluetoothScanMethod);
+            // startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
---
+++ AndroidApp/app/src/main/java/com/romanckua/carcanremote/ScanningBluetooth.java
@@ -0,0 +1,140 @@
+package com.romanckua.carcanremote;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.*;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class ScanningBluetooth extends AppCompatActivity {
+
+    BluetoothLeScanner scanner;
+    BluetoothAdapter bluetoothAdapter;
+    BluetoothDevice device;
+    List<ScanFilter> filters = null;
+    ListView listView;
+    HashSet<String> hashSetBTaddList = new HashSet<>();
+    ArrayList<String> listViewBTlist = new ArrayList<>();
+    ArrayList<String> macAddressList = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+    TextView scanBTListTextView;
+
+    private final ScanCallback scanCallback = new ScanCallback() {
+
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            device = result.getDevice();
+            System.out.println(device);
+            if (hashSetBTaddList != null) {
+                if (!hashSetBTaddList.contains(device.getAddress())) {
+                    hashSetBTaddList.add(device.getAddress());
+                    listViewBTlist.add(device.getName() + ": " + device.getAddress());
+                    macAddressList.add(device.getAddress());
+                    adapter.notifyDataSetChanged();
+                }
+            } else {
+
+                hashSetBTaddList.add(device.getAddress());
+                listViewBTlist.add(device.getName() + ": " + device.getAddress());
+                macAddressList.add(device.getAddress());
+                adapter.notifyDataSetChanged();
+            }
+
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        if (scanner != null) {
+            scanner.stopScan(scanCallback);
+        }
+        super.onPause();
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scanningbluetooth);
+        setTitle("Сканую наявні пристрої Bluetooth");
+        listView = findViewById(R.id.scanBTList);
+        scanBTListTextView = findViewById(R.id.scanBTTextView0);
+
+        if (listViewBTlist != null) {
+            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listViewBTlist);
+            listView.setAdapter(adapter);
+        }
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        scanner = bluetoothAdapter.getBluetoothLeScanner();
+
+        ScanSettings scanSettings = new ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
+                .setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES)
+                .setMatchMode(ScanSettings.MATCH_MODE_AGGRESSIVE)
+                .setNumOfMatches(ScanSettings.MATCH_NUM_ONE_ADVERTISEMENT)
+                .setReportDelay(0L)
+                .build();
+
+
+        if (scanner != null) {
+            scanner.startScan(filters, scanSettings, scanCallback);
+        } else { }
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View itemClicked, int position, long id) {
+                String data = macAddressList.get(position);
+                scanBTListTextView.setText(data);
+
+
+                FileOutputStream fos = null;
+                try {
+                    fos = openFileOutput("setting.cfg", MODE_PRIVATE);
+                    fos.write(data.getBytes());
+                }
+                catch(IOException ex) {
+                    ex.getMessage();
+                }
+                finally{
+                    try{
+                        if(fos!=null)
+                            fos.close();
+                    }
+                    catch(IOException ex){
+                        ex.getMessage();
+                    }
+                }
+                Intent intent = new Intent(MainActivity.BROADCAST_ACTION);
+                intent.putExtra(MainActivity.PARAM_MSG, "reconnect");
+                sendBroadcast(intent);
+                onPause();
+            }
+        });
+
+    }
+
+}
+
